60fe14f8322f8f85067a658562b6817d7743c0be
Isai Chaidez <isaichaidez@gmail.com>
2023-08-10 11:07:21 -0700

    fixed issues with website, everything now works
    yes
    yes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+//prompts for user input
 let userColor = prompt("Hello! What color would you like the website to be: black or white?");
 let userSong = prompt("Whats your favorite song?");
 
@@ -7,11 +9,10 @@ let youtube = document.getElementById("youtube");
 let arvoya = document.getElementById("arvoya");
 let soundcloud = document.getElementById("soundcloud");
 let button = document.getElementById("button");
-// console.log("test");
-// youtube.style.hover ="red";
-// console.log("test2");
 
-// changes colors based on choice
+//***future change is to use class"links" instead of indivudal list items. 
+// look up function that changes strings to all lower case 
+// changes website color based on choice
 if (userColor === "white" || userColor === "White" || userColor === "WHITE") {
     document.body.style.backgroundColor = "white";
     document.body.style.color = "black";
@@ -19,7 +20,7 @@ if (userColor === "white" || userColor === "White" || userColor === "WHITE") {
     arvoya.style.color = "black";
     soundcloud.style.color = "black";
     button.style.color = "black";
-    // function Handle.mous
+
 } else if (userColor === "black" || userColor === "Black" || userColor === "BLACK") {
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
@@ -36,6 +37,18 @@ if (userColor === "white" || userColor === "White" || userColor === "WHITE") {
 }
 
 
+// if no song was inputed
+if (userSong === ""){
+    console.log(userSong);
+    alert("Please put in a song!");
+    let songRequest = document.createElement("p");
+    songRequest.textContent = "A link to your favorite song could have been here!!!"
+    //append the new text to div section
+    let divSection = document.querySelector("div");
+    divSection.appendChild(songRequest);
+
+} else {
+//if song was inputed
 let favoriteSongLink = document.createElement("a");
 favoriteSongLink.href = "https://www.youtube.com/results?search_query=" + encodeURIComponent(userSong);
 favoriteSongLink.target = "_blank";
@@ -46,6 +59,9 @@ favoriteSongLink.style.color = document.body.style.color;
 let mainSection = document.querySelector("main");
 document.write(userSong + "! I love " + userSong + "!");
 mainSection.appendChild(favoriteSongLink);
+
+}
+
 
 // Add event listeners for hover effect
 youtube.addEventListener("mouseover", function() {
