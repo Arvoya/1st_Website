5107bcf97372e287888ce84d8b8fabeb723cde4c
Isai Chaidez <isaichaidez@gmail.com>
2023-08-09 23:43:09 -0700

    debugged some stuff that wasn't working

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let userSong = prompt("Whats your favorite song?");
 let youtube = document.getElementById("youtube");
 let arvoya = document.getElementById("arvoya");
 let soundcloud = document.getElementById("soundcloud");
+let button = document.getElementById("button");
 // console.log("test");
 // youtube.style.hover ="red";
 // console.log("test2");
@@ -17,6 +18,7 @@ if (userColor === "white" || userColor === "White" || userColor === "WHITE") {
     youtube.style.color = "black";
     arvoya.style.color = "black";
     soundcloud.style.color = "black";
+    button.style.color = "black";
     // function Handle.mous
 } else if (userColor === "black" || userColor === "Black" || userColor === "BLACK") {
     document.body.style.backgroundColor = "black";
@@ -24,6 +26,7 @@ if (userColor === "white" || userColor === "White" || userColor === "WHITE") {
     youtube.style.color = "white";
     arvoya.style.color = "white";
     soundcloud.style.color = "white";
+    button.style.color = "white";
 } else {
     // Handle cases where the user input is neither "white" nor "black"
     alert("Invalid color choice. Please choose either 'white' or 'black'.");
@@ -33,7 +36,39 @@ if (userColor === "white" || userColor === "White" || userColor === "WHITE") {
 }
 
 
+let favoriteSongLink = document.createElement("a");
+favoriteSongLink.href = "https://www.youtube.com/results?search_query=" + encodeURIComponent(userSong);
+favoriteSongLink.target = "_blank";
+favoriteSongLink.textContent = "Click Me";
+favoriteSongLink.style.color = document.body.style.color;
 
-console.log("test");
-youtube.style.hover ="red";
-console.log("test2");
+// Append the new link to the main section
+let mainSection = document.querySelector("main");
+document.write(userSong + "! I love " + userSong + "!");
+mainSection.appendChild(favoriteSongLink);
+
+// Add event listeners for hover effect
+youtube.addEventListener("mouseover", function() {
+    this.style.color = "red";
+});
+
+youtube.addEventListener("mouseout", function() {
+    
+    this.style.color = document.body.style.color;
+});
+
+arvoya.addEventListener("mouseover", function() {
+    this.style.color = "green";
+})
+
+arvoya.addEventListener("mouseout", function() {
+    this.style.color = document.body.style.color;
+})
+
+soundcloud.addEventListener("mouseover", function() {
+    this.style.color = "orange";
+})
+
+soundcloud.addEventListener("mouseout", function() {
+    this.style.color = document.body.style.color;
+})
